8411704f248cfa50946fd9c8f3ab84e842c224df
Blackburn0 <hamzatadebayo5@gmail.com>
2024-08-04 11:14:41 +0100

    Initial commit

---
+++ portfolio/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import About, Expereince, Education, Project, Contact
+# Register your models here.
+
+admin.site.register(About)
+admin.site.register(Expereince)
+admin.site.register(Education)
+admin.site.register(Project)
+admin.site.register(Contact)
